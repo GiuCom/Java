7c135ed5f89c9f905d19985a4cec849cd53e26f4
GiuCom <giuseppe@compagno.cloud>
2026-02-25 17:16:15 +0100

    Completato: - Appunti: JVM
    Aggiunte immagini:
    - JVM_Architettura_EXECUTION.png
    - JVM_Architettura_LOADER.png
    - JVM_Architettura_RUNTIME.png

--- designpatterns/creazionali/sigleton/src/main/java/cloud/compagno/designpatterns/crazionali/singleton/SingletonMain.java
+++ designpatterns/creazionali/sigleton/src/main/java/cloud/compagno/designpatterns/crazionali/singleton/SingletonMain.java
@@ -1,7 +1,26 @@
 package cloud.compagno.designpatterns.crazionali.singleton;
 
 public class SingletonMain {
+
     static void main() {
-        System.out.println("Hello and welcome!");
+        System.out.println("--- Inizio Programma ---");
+
+        // Prima chiamata: scatena l'inizializzazione del Singleton
+        System.out.println("Richiedo la prima istanza...");
+        SingletonBillPugh config1 = SingletonBillPugh.getInstance();
+        System.out.println("Stringa di connessione: " + config1.getConnectionString());
+
+        // Seconda chiamata: non vedrai il messaggio di inizializzazione
+        System.out.println("\nRichiedo la seconda istanza...");
+        SingletonBillPugh config2 = SingletonBillPugh.getInstance();
+
+        // Verifica finale via console
+        if (config1 == config2) {
+            System.out.println("SUCCESSO: Entrambe le variabili puntano alla stessa istanza (HashCode: " + config1.hashCode() + ")");
+        } else {
+            System.out.println("ERRORE: Le istanze sono diverse!");
+        }
+
+        System.out.println("--- Fine Programma ---");
     }
 }
