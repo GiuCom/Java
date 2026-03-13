d134b47b7428a637593b1165752d13923552aab6
GiuCom <giuseppe@compagno.cloud>
2026-03-13 12:41:12 +0100

    Creazione nuovo design patterns Factory Method README.md Codice sorgente Test

---
+++ designpatterns/creazionali/factorymethod/src/main/java/cloud/compagno/designpatterns/creazionali/factorymethod/FactoryMethodMain.java
@@ -0,0 +1,9 @@
+package cloud.compagno.designpatterns.creazionali.factorymethod;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class FactoryMethodMain {
+    static void main() {
+
+    }
+}
